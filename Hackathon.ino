8d6b0eeab63e09d68e3bf85388f186c69d6feb52
dohyeon kim <xingchen8837@gmail.com>
2025-09-17 17:20:22 +0900

    MPU-92 SoftSPI 변경

--- Hackathon.ino
+++ Hackathon.ino
@@ -1,14 +1,18 @@
+#define SERIAL_BAUD 115200
+#define GPS_BAUD 9600L
+#define GPS_RX_PIN 3
+#define GPS_TX_PIN 4 
+#define SD_CS_PIN 10
+#define MPU9250_CS_PIN 9
+
+#define USE_SOFT_SPI
+
 #include "GPSdata.h"
 #include "SDDataLogger.h"
 #include "MotionData.h"
 #include "RTC.h"
 
-#define SERIAL_BAUD 115200
-#define GPS_BAUD 9600L
-#define GPS_RX_PIN 3
-#define GPS_TX_PIN 4 
-#define SD_CS_PIN 9
-#define MPU9250_CS_PIN 8
+
 
 GPSdata gpsData(GPS_RX_PIN, GPS_TX_PIN);
 SDDataLogger sdLogger(SD_CS_PIN);
--- MotionData.h
+++ MotionData.h
@@ -3,110 +3,173 @@
 
 #include <Arduino.h>
 #include <SPI.h>
-#include <Wire.h>
+
+// Define USE_SOFT_SPI to enable software SPI
+// #define USE_SOFT_SPI
+
+#ifdef USE_SOFT_SPI
+// Software SPI default pins
+#define SOFT_SCK_PIN 5
+#define SOFT_MOSI_PIN 6
+#define SOFT_MISO_PIN 7
+#endif
 
 // MPU-9250 Registers
 #define MPU9250_WHO_AM_I      0x75
 #define MPU9250_PWR_MGMT_1    0x6B
 #define MPU9250_ACCEL_XOUT_H  0x3B
 #define MPU9250_GYRO_XOUT_H   0x43
-
 #define MPU9250_GYRO_CONFIG   0x1B
-
-// USER_CTRL 레지스터
+#define MPU9250_ACCEL_CONFIG  0x1C
 #define MPU9250_USER_CTRL     0x6A
 #define I2C_IF_DIS            0x10  // Bit4
 
-// AK8963 (Magnetometer) Registers
-#define AK8963_ADDR           0x0C
-#define AK8963_WHO_AM_I       0x00
-#define AK8963_CNTL1          0x0A
-#define AK8963_ST1            0x02
-#define AK8963_XOUT_L         0x03
-
-// MPU-92 SPI pins
-// SCLK: 1
-// SDA/SDI: MOSI 11
-// ADO/SDO: MISO 12
-
 class MotionData {
 public:
-    MotionData(uint8_t csPin = 9);
+    MotionData(uint8_t csPin);
     bool begin();
     void readAccel(float &ax, float &ay, float &az);
     void readGyro(float &gx, float &gy, float &gz);
-    // void readMag(float &mx, float &my, float &mz);
 
 private:
     uint8_t _csPin;
+
+#ifndef USE_SOFT_SPI
+    SPIClass* _spi;
+#else
+    uint8_t _sckPin;
+    uint8_t _mosiPin;
+    uint8_t _misoPin;
+
+    void softSPIBegin();
+    uint8_t softSPITransfer(uint8_t data);
+#endif
+
     void writeRegister(uint8_t reg, uint8_t value);
     uint8_t readRegister(uint8_t reg);
-    void readRegisters(uint8_t reg, uint8_t *buffer, uint8_t len);
-
-    // Magnetometer helpers
-    // void writeMagRegister(uint8_t reg, uint8_t value);
-    // uint8_t readMagRegister(uint8_t reg);
-    // void readMagRegisters(uint8_t reg, uint8_t *buffer, uint8_t len);
+    void readRegisters(uint8_t reg, uint8_t* buffer, uint8_t length);
 };
 
-MotionData::MotionData(uint8_t csPin) : _csPin(csPin) {}
+// Constructor
+MotionData::MotionData(uint8_t csPin) 
+    : _csPin(csPin)
+#ifndef USE_SOFT_SPI
+    , _spi(&SPI)
+#else
+    , _sckPin(SOFT_SCK_PIN), _mosiPin(SOFT_MOSI_PIN), _misoPin(SOFT_MISO_PIN)
+#endif
+{}
 
+// Begin
 bool MotionData::begin() {
     pinMode(_csPin, OUTPUT);
     digitalWrite(_csPin, HIGH);
-    SPI.begin();
 
-    // Wake up MPU-9250
-    writeRegister(MPU9250_PWR_MGMT_1, 0x00);
+#ifndef USE_SOFT_SPI
+    _spi->begin();
+#else
+    softSPIBegin();
+#endif
+
+    // Reset and initialize MPU-9250
+    writeRegister(MPU9250_PWR_MGMT_1, 0x80); // Reset
     delay(100);
-
-    // I2C 인터페이스 비활성화
-    writeRegister(MPU9250_USER_CTRL, I2C_IF_DIS);
+    writeRegister(MPU9250_PWR_MGMT_1, 0x01); // Wake up
     delay(10);
+    writeRegister(MPU9250_GYRO_CONFIG, 0x00); // ±250 dps
+    writeRegister(MPU9250_ACCEL_CONFIG, 0x00); // ±2g
 
-    writeRegister(MPU9250_GYRO_CONFIG, 0x00); // ±250°/s
-
-    // Check WHO_AM_I
+    // Optional: Check WHO_AM_I
     uint8_t whoami = readRegister(MPU9250_WHO_AM_I);
-
-    Serial.print("***********\nMPU9250 WHO_AM_I: 0x");
-    Serial.print(whoami, HEX);
-    Serial.println("\n***********");
-
-    if (whoami != 0x71) return false;
-
-    // Magnetometer init (pass-through mode)
-    // writeRegister(0x37, 0x02); // Enable bypass
-    // delay(10);
-    // writeMagRegister(AK8963_CNTL1, 0x16); // 16-bit, continuous mode 2
-    // delay(10);
+    Serial.print("MPU9250 WHO_AM_I: 0x");
+    Serial.println(whoami, HEX);
 
     return true;
 }
 
+#ifndef USE_SOFT_SPI
+// Hardware SPI
+void MotionData::writeRegister(uint8_t reg, uint8_t value) {
+    _spi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    digitalWrite(_csPin, LOW);
+    _spi->transfer(reg & 0x7F);
+    _spi->transfer(value);
+    digitalWrite(_csPin, HIGH);
+    _spi->endTransaction();
+}
+
+uint8_t MotionData::readRegister(uint8_t reg) {
+    uint8_t value;
+    _spi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    digitalWrite(_csPin, LOW);
+    _spi->transfer(reg | 0x80);
+    value = _spi->transfer(0x00);
+    digitalWrite(_csPin, HIGH);
+    _spi->endTransaction();
+    return value;
+}
+
+void MotionData::readRegisters(uint8_t reg, uint8_t* buffer, uint8_t length) {
+    _spi->beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+    digitalWrite(_csPin, LOW);
+    _spi->transfer(reg | 0x80);
+    for (uint8_t i = 0; i < length; i++) {
+        buffer[i] = _spi->transfer(0x00);
+    }
+    digitalWrite(_csPin, HIGH);
+    _spi->endTransaction();
+}
+
+#else
+// Software SPI
+void MotionData::softSPIBegin() {
+    pinMode(_sckPin, OUTPUT);
+    pinMode(_mosiPin, OUTPUT);
+    pinMode(_misoPin, INPUT);
+    digitalWrite(_sckPin, LOW);
+}
+
+uint8_t MotionData::softSPITransfer(uint8_t data) {
+    uint8_t received = 0;
+    for (int8_t i = 7; i >= 0; i--) {
+        digitalWrite(_sckPin, LOW);
+        digitalWrite(_mosiPin, (data & (1 << i)) ? HIGH : LOW);
+        delayMicroseconds(1);
+        digitalWrite(_sckPin, HIGH);
+        delayMicroseconds(1);
+        received <<= 1;
+        if (digitalRead(_misoPin)) {
+            received |= 0x01;
+        }
+    }
+    digitalWrite(_sckPin, LOW);
+    return received;
+}
+
 void MotionData::writeRegister(uint8_t reg, uint8_t value) {
     digitalWrite(_csPin, LOW);
-    SPI.transfer(reg & 0x7F);
-    SPI.transfer(value);
+    softSPITransfer(reg & 0x7F);
+    softSPITransfer(value);
     digitalWrite(_csPin, HIGH);
 }
 
 uint8_t MotionData::readRegister(uint8_t reg) {
     digitalWrite(_csPin, LOW);
-    SPI.transfer(reg | 0x80);
-    uint8_t value = SPI.transfer(0x00);
+    softSPITransfer(reg | 0x80);
+    uint8_t value = softSPITransfer(0x00);
     digitalWrite(_csPin, HIGH);
     return value;
 }
 
-void MotionData::readRegisters(uint8_t reg, uint8_t *buffer, uint8_t len) {
+void MotionData::readRegisters(uint8_t reg, uint8_t* buffer, uint8_t length) {
     digitalWrite(_csPin, LOW);
-    SPI.transfer(reg | 0x80);
-    for (uint8_t i = 0; i < len; i++) {
-        buffer[i] = SPI.transfer(0x00);
+    softSPITransfer(reg | 0x80);
+    for (uint8_t i = 0; i < length; i++) {
+        buffer[i] = softSPITransfer(0x00);
     }
     digitalWrite(_csPin, HIGH);
 }
+#endif
 
 void MotionData::readAccel(float &ax, float &ay, float &az) {
     uint8_t buf[6];
@@ -129,50 +192,5 @@ void MotionData::readGyro(float &gx, float &gy, float &gz) {
     gy = y / 131.0f;
     gz = z / 131.0f;
 }
-
-// Magnetometer via I2C pass-through
-/*
-void MotionData::writeMagRegister(uint8_t reg, uint8_t value) {
-    Wire.beginTransmission(AK8963_ADDR);
-    Wire.write(reg);
-    Wire.write(value);
-    Wire.endTransmission();
-}
-
-uint8_t MotionData::readMagRegister(uint8_t reg) {
-    Wire.beginTransmission(AK8963_ADDR);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    Wire.requestFrom(AK8963_ADDR, 1);
-    return Wire.read();
-}
-
-void MotionData::readMagRegisters(uint8_t reg, uint8_t *buffer, uint8_t len) {
-    Wire.beginTransmission(AK8963_ADDR);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    Wire.requestFrom(AK8963_ADDR, len);
-    for (uint8_t i = 0; i < len; i++) {
-        buffer[i] = Wire.read();
-    }
-}
-
-void MotionData::readMag(float &mx, float &my, float &mz) {
-    uint8_t st1 = readMagRegister(AK8963_ST1);
-    if (!(st1 & 0x01)) {
-        mx = my = mz = 0;
-        return;
-    }
-    uint8_t buf[6];
-    readMagRegisters(AK8963_XOUT_L, buf, 6);
-    int16_t x = (buf[1] << 8) | buf[0];
-    int16_t y = (buf[3] << 8) | buf[2];
-    int16_t z = (buf[5] << 8) | buf[4];
-    mx = x * 0.15f;
-    my = y * 0.15f;
-    mz = z * 0.15f;
-}
-*/
-
 
 #endif // MOTIONDATA_H
