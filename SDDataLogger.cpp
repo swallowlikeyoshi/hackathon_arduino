ff810aee8261755d66ed9d117180a3013b037ac7
dohyeon kim <xingchen8837@gmail.com>
2025-09-22 03:34:24 +0900

    SD 로그 파일 이름 동적 저장 (미완)

--- SDDataLogger.cpp
+++ SDDataLogger.cpp
@@ -3,8 +3,8 @@
 SDDataLogger::SDDataLogger(uint8_t csPin)
     : chipSelect(csPin), fileOpen(false) {}
 
-bool SDDataLogger::begin(const String& fileName) {
-    this->fileName = fileName;
+bool SDDataLogger::begin() {
+    this->fileName = "datalog_" + String(fileCount) + ".txt";
     return SD.begin(chipSelect);
 }
 
@@ -14,22 +14,40 @@ bool SDDataLogger::open(uint8_t mode) {
     }
     file = SD.open(fileName, mode);
     fileOpen = file ? true : false;
+    Serial.println("file status: " + String(fileOpen ? "opened" : "failed to open"));
     return fileOpen;
 }
 
-bool SDDataLogger::setFileName(const String& fileName) {
+bool SDDataLogger::setFileName(const String& filePrefix) {
     if (fileOpen) {
         file.close();
         fileOpen = false;
     }
-    this->fileName = fileName;
-    return open();
+    this->filePrefix = filePrefix;
+    this->fileName = filePrefix + String(fileCount) + ".txt";
+    if (open(FILE_WRITE)) {
+        logCount = 0; // Reset log count for new file
+        return true;
+    }
+
+    return false;
 }
 
 bool SDDataLogger::log(const String& data) {
+    if (logCount >= MAX_LOG_ENTRIES) {
+        fileCount++;
+        String newFileName = filePrefix + String(fileCount) + ".txt";
+        this->fileName = newFileName;
+        if (open(FILE_WRITE)) {
+            logCount = 0;
+            file.println(fileName + " Log Start");
+            file.flush();
+        }
+    }
     if (fileOpen && file) {
         file.println(data);
         file.flush();
+        logCount++;
         return true;
     }
     return false;
